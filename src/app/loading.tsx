2658ee5336ac5b3c9b1918e98085f7e9f9757ab0
Richard Oropesa <richardoro2001@gmail.com>
2024-04-18 21:31:52 -0400

    feat: loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,14 @@
+'use client';
+
+import { Spinner } from '@nextui-org/react';
 import React from 'react';
 
 const Loading = () => {
-  return <div>Loading...</div>;
+  return (
+    <div className='w-screen h-screen flex items-center justify-center fixed top-0 left-0 backdrop-blur-sm'>
+      <Spinner />
+    </div>
+  );
 };
 
 export default Loading;
