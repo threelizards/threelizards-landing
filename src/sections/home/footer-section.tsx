e8a4c3bf9feb050356c7366728134e04d7d0b7d9
Javy011021 <garciaj1246@gmail.com>
2024-06-09 16:50:03 -0400

    feat: footer-mobile

--- src/sections/home/footer-section.tsx
+++ src/sections/home/footer-section.tsx
@@ -8,21 +8,21 @@ import React from 'react';
 const FooterSection = async () => {
   const { t } = await useTranslationServer('footer-section');
   return (
-    <footer className='w-full p-4 '>
-      <div className='grid-cols-2 justify-center lg:items-center lg:flex lg:justify-between'>
+    <footer className='w-full p-4 max-w-7xl '>
+      <div className='justify-center lg:items-center lg:flex lg:justify-between'>
         <Social />
-        <div className='flex flex-col mt-4 text-center lg:flex-row lg:mt-4 lg:text-center'>
-          <H5 className='mb-2 text-zinc-500'>{t('rights')}</H5>
-          <H5 className='mb-2 text-zinc-500'>
-            <Link href={routes.terms} className='ml-10'>
-              {t('terms')}
-            </Link>
-          </H5>
-          <H5 className='mb-2 text-zinc-500'>
-            <Link href={routes.policy} className='ml-10'>
-              {t('policy')}
-            </Link>
-          </H5>
+        <div className='mt-4 text-center gap-x-5 lg:flex-row lg:mt-4 lg:text-center lg:flex lg:justify-between md:flex-row md:mt-4 md:text-center md:flex md:justify-between'>
+          <div>
+            <H5 className='mb-2 text-zinc-500'>{t('rights')}</H5>
+          </div>
+          <div className='flex justify-center flex-col items-center lg:flex-row lg:space-x-4 md:flex md:flex-row md:space-x-4'>
+            <H5 className='mb-2 text-zinc-500'>
+              <Link href={routes.terms}>{t('terms')}</Link>
+            </H5>
+            <H5 className='mb-2 text-zinc-500'>
+              <Link href={routes.policy}>{t('policy')}</Link>
+            </H5>
+          </div>
         </div>
       </div>
     </footer>
