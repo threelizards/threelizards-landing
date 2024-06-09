293f42009718ff7e14e55dda0d13f08b2dd9fcfe
Richard Oropesa <richardoro2001@gmail.com>
2024-06-09 16:21:08 -0400

    feat: presentation section mobile

--- src/sections/home/presentation-section.tsx
+++ src/sections/home/presentation-section.tsx
@@ -9,17 +9,17 @@ const PresentationSection = async () => {
   const { t } = await useTranslationServer('presentation-section');
 
   return (
-    <section className='flex gap-24 max-w-7xl items-center px-8 my-10'>
-      <div className='flex flex-col w-full gap-6'>
+    <section className='flex flex-col lg:flex-row gap-6 lg:gap-24 max-w-7xl items-center px-8 my-10'>
+      <div className='flex flex-col w-full gap-6 text-center lg:text-start items-center lg:items-start'>
         <H1>
           {t('title')} <span className='text-secondary2'>{t('title2')}</span>
         </H1>
         <Subtitle>{t('subtitle')}</Subtitle>
-        <Button color='success' variant='solid' className='max-w-40'>
+        <Button color='success' variant='solid' className='max-w-40' size='lg'>
           {t('contact')}
         </Button>
       </div>
-      <div className='relative w-full'>
+      <div className='relative w-full flex flex-col items-center lg:items-start'>
         <Image src={girlPhoto} alt='Worker girl' />
       </div>
     </section>
