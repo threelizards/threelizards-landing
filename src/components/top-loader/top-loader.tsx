c37e6eff20e679854889bf0475fcb372195a0a59
Richard Oropesa <richardoro2001@gmail.com>
2024-10-25 13:13:47 -0400

    fix: top loader hydration

--- src/components/top-loader/top-loader.tsx
+++ src/components/top-loader/top-loader.tsx
@@ -1,13 +1,23 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import NextTopLoader from 'nextjs-toploader';
 import { useTheme } from 'next-themes';
 import { palette } from '@/theme/palette';
 
-const TopLoader: React.FC = () => {
+const TopLoaderClient: React.FC = () => {
   const { theme } = useTheme();
-  return <NextTopLoader color={theme === 'dark' ? palette.primary[500] : palette.primary[400]} />;
+  return <NextTopLoader color={theme === 'dark' ? palette.primary[500] : palette.primary[400]} showSpinner={false} />;
+};
+
+const TopLoader: React.FC = () => {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (mounted) return <TopLoaderClient />;
+  return <NextTopLoader color={palette.primary[500]} showSpinner={false} />;
 };
 
 export default TopLoader;
